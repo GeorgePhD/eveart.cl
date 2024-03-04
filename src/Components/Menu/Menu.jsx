0d38ef38c66930534d7222d262306ec1c487ae03
George <lets.devel0pme@gmail.com>
2024-03-04 19:13:45 -0300

    icons added

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -14,9 +14,9 @@ const Menu = () => {
     return (
         <>
             <Navbar expand="md" className="bg-body-tertiary mb-3" data-bs-theme="light">
-                <img src={Logo} className='logo' alt="Logo T.Protein" /><h4 className='menu__eve'>EveArt, Artista.</h4>
+                <img src={Logo} className='logo' alt="Logo EveArt"/><h4 className='menu__eve'>EveArt, Artista.</h4>
                 <Container fluid>
-                    <Navbar.Brand className='navbar__brand' href="#">{/* T.Protein */}</Navbar.Brand>
+                    <Navbar.Brand className='navbar__brand' href="#"></Navbar.Brand>
                     <Navbar.Toggle aria-controls="offcanvasNavbar" />
                     <Navbar.Offcanvas
                         id="offcanvasNavbar"
@@ -30,12 +30,6 @@ const Menu = () => {
                             <Nav className="justify-content-end flex-grow-1 pe-3">
                                 <Nav.Link className='nav__link' as={Link} to="./home">Inicio</Nav.Link>
                                 <Nav.Link className='nav__link' as={Link} to="./about">Acerca de mi</Nav.Link>
-                                {/* <Nav.Link className='nav__link' as={Link} to="./protein">Proteínas</Nav.Link>
-                                <Nav.Link className='nav__link' as={Link} to="./creatine">Creatina</Nav.Link>
-                                <Nav.Link className='nav__link' as={Link} to="./aminoacids">Aminoácidos</Nav.Link>
-                                <Nav.Link className='nav__link' as={Link} to="./preworkout">Pre-entrenos</Nav.Link>
-                                <Nav.Link className='nav__link' as={Link} to="./accessories">Accesorios</Nav.Link>
-                                <Nav.Link className='nav__link' as={Link} to="./gear">indumentaria deportiva</Nav.Link> */}
                                 <NavDropdown
                                     className='nav__link'
                                     title="Galería"
@@ -45,12 +39,6 @@ const Menu = () => {
                                     <NavDropdown.Item className='nav__link' as={Link} to="./creatine">
                                     Art 2
                                     </NavDropdown.Item>
-                                    {/* <NavDropdown.Item className='nav__link' as={Link} to="./aminoacids">
-                                    Aminoácidos
-                                    </NavDropdown.Item>
-                                    <NavDropdown.Item className='nav__link' as={Link} to="./preworkout">
-                                    Pre-entrenos
-                                    </NavDropdown.Item> */}
                                     <NavDropdown.Item className='nav__link' as={Link} to="./accessories">
                                     Art 3
                                     </NavDropdown.Item>
@@ -59,15 +47,6 @@ const Menu = () => {
                                     </NavDropdown.Item>
                                 </NavDropdown>
                             </Nav>
-                            {/* <Form className="d-flex">
-                                <Form.Control
-                                    type="search"
-                                    placeholder="Search"
-                                    className="me-2"
-                                    aria-label="Search"
-                                />
-                                <Button variant="outline-success">Search</Button>
-                            </Form> */}
                         </Offcanvas.Body>
                     </Navbar.Offcanvas>
                 </Container>
