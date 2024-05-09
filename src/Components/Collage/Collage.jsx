8f9f49396d118cb8e93f0d9c1840246f2079fe3c
George <lets.devel0pme@gmail.com>
2024-05-09 10:28:29 -0400

    update: collage section P styles changed

--- src/Components/Collage/Collage.jsx
+++ src/Components/Collage/Collage.jsx
@@ -52,132 +52,99 @@ const Collage = () => {
         <div className='grid__layout__collage'>
             <section className="collage__container">
                 <img className='collage__img' src={Image1} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando XXXIII, 2013.</h2>
-                <p className="collage__p">Madera, fósforo, dado y fotografías
-                    15 cm x 22,5 cm.
-                </p>
+                <p className="collage__p">La poesía se hace tachando XXXIII, 2013. Madera, fósforo, dado y fotografías
+                    15 cm x 22,5 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image2} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando III, 2013.</h2>
-                <p className="collage__p">Recortes de revista y plástico, 10 cm x 10 cm.</p>
+                <p className="collage__p">La poesía se hace tachando III, 2013. Recortes de revista y plástico, 10 cm x 10 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image3} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando VII, 2013. </h2>
-                <p className="collage__p">Recortes de revista, cinta adhesiva, cartón, 10 cm x 15 cm.</p>
+                <p className="collage__p">La poesía se hace tachando VII, 2013. Recortes de revista, cinta adhesiva, cartón, 10 cm x 15 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image4} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando VI, 2013.</h2>
-                <p className="collage__p">Recortes de revista, tinta y cartón, 11 cm x 14,5 cm.</p>
+                <p className="collage__p">La poesía se hace tachando VI, 2013. Recortes de revista, tinta y cartón, 11 cm x 14,5 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image5} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando IX, 2013.</h2>
-                <p className="collage__p">Vidrio, plástico y recortes de láminas de libros de Historia del Arte , 12 cm x 14 cm.</p>
+                <p className="collage__p">La poesía se hace tachando IX, 2013. Vidrio, plástico y recortes de láminas de libros de Historia del Arte , 12 cm x 14 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image6} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando XIV, 2013. </h2>
-                <p className="collage__p">Recortes de revista, 14 cm x 21 cm.</p>
+                <p className="collage__p">La poesía se hace tachando XIV, 2013. Recortes de revista, 14 cm x 21 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image7} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando XIX, 2013. </h2>
-                <p className="collage__p">Recortes de fotografías, periódico, tinta y madera.</p>
+                <p className="collage__p">La poesía se hace tachando XIX, 2013. Recortes de fotografías, periódico, tinta y madera.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image8} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando, 2013.</h2>
-                <p className="collage__p">Recortes de revistas y de libros de historia del Arte, vidrio, madera y pintura acrílica, 30 cm x 20 cm.</p>
+                <p className="collage__p">La poesía se hace tachando, 2013. Recortes de revistas y de libros de historia del Arte, vidrio, madera y pintura acrílica, 30 cm x 20 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image9} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando XXV, 2013.</h2>
-                <p className="collage__p">Lámina de libro, madera, 24 cm x 25 cm.</p>
+                <p className="collage__p">La poesía se hace tachando XXV, 2013. Lámina de libro, madera, 24 cm x 25 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image10} alt="collage" />
-                <h2 className="collage__subtitle">Regalo catorce, 2014.</h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta, pintura acrílica y papel,
-                    18 cm x 22 cm.
-                </p>
+                <p className="collage__p">Regalo catorce, 2014. Lámina de libro de Historia del Arte, tinta, pintura acrílica y papel,
+                    18 cm x 22 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image11} alt="collage" />
-                <h2 className="collage__subtitle">Regalo dieciséis, 2014.</h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    15,2 cm x 21,6 cm.
-                </p>
+                <p className="collage__p">Regalo dieciséis, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    15,2 cm x 21,6 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image12} alt="collage" />
-                <h2 className="collage__subtitle">Regalo diez, 2014. </h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    15,2 cm x 21,6 cm.
-                </p>
+                <p className="collage__p">Regalo diez, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    15,2 cm x 21,6 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image13} alt="collage" />
-                <h2 className="collage__subtitle">Regalo doce, 2014. </h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    15,2 cm x 22 cm.
-                </p>
+                <p className="collage__p">Regalo doce, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    15,2 cm x 22 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image14} alt="collage" />
-                <h2 className="collage__subtitle">Regalo dos, 2014. </h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    15,8 cm x 21 cm.
-                </p>
+                <p className="collage__p">Regalo dos, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    15,8 cm x 21 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image15} alt="collage" />
-                <h2 className="collage__subtitle">Regalo nueve, 2014. </h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    17 cm x 25 cm.
-                </p>
+                <p className="collage__p">Regalo nueve, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    17 cm x 25 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image16} alt="collage" />
-                <h2 className="collage__subtitle">Regalo ocho, 2014.</h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    22 cm x 18,5 cm.
-                </p>
+                <p className="collage__p">Regalo ocho, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    22 cm x 18,5 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image17} alt="collage" />
-                <h2 className="collage__subtitle">Regalo seis, 2014. </h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    21 cm x 14,7 cm.
-                </p>
+                <p className="collage__p">Regalo seis, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    21 cm x 14,7 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image18} alt="collage" />
-                <h2 className="collage__subtitle">Regalo trece, 2014. </h2>
-                <p className="collage__p">Láminas de libro de Historia del Arte, tinta y papel,
-                    20 cm x 27 cm.
-                </p>
+                <p className="collage__p">Regalo trece, 2014. Láminas de libro de Historia del Arte, tinta y papel,
+                    20 cm x 27 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image19} alt="collage" />
-                <h2 className="collage__subtitle"></h2>
                 <p className="collage__p">Description</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image20} alt="collage" />
-                <h2 className="collage__subtitle">Regalo uno, 2014. </h2>
-                <p className="collage__p">Lámina de libro de Historia del Arte, tinta y papel,
-                    15,2 cm x 21,6 cm.
-                </p>
+                <p className="collage__p">Regalo uno, 2014. Lámina de libro de Historia del Arte, tinta y papel,
+                    15,2 cm x 21,6 cm.</p>
             </section>
             <section className="collage__container">
                 <img className='collage__img' src={Image21} alt="collage" />
-                <h2 className="collage__subtitle">La poesía se hace tachando, 2013. </h2>
-                <p className="collage__p">Vidrio, pintura de óleo, lámina de libre de Historia del Arte, 100 cm x 70 cm.</p>
+                <p className="collage__p">La poesía se hace tachando, 2013. Vidrio, pintura de óleo, lámina de libre de Historia del Arte, 100 cm x 70 cm.</p>
             </section>
-
         </div>
     )
 }
