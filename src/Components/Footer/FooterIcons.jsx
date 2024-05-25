532bae169f2f0dc57929bebaec4bfa90ca8a6346
George <lets.devel0pme@gmail.com>
2024-05-24 20:22:29 -0400

    update: artsy logo added

--- src/Components/Footer/FooterIcons.jsx
+++ src/Components/Footer/FooterIcons.jsx
@@ -2,9 +2,9 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faFacebook, faInstagram, faWhatsapp, faYoutube, faTiktok } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link } from 'react-router-dom';
-
+import artsyLogo from '../../images/artsy-logo.png';
 library.add(faFacebook, faInstagram, faWhatsapp, faYoutube, faTiktok);
-/* const facebook = "https://www.facebook.com/profile.php?id=61550567625975&mibextid=ZbWKwL"; */
+const artsy = "https://www.artsy.net/artist/evelyn-aguilar-sanchez";
 const instagram = "https://www.instagram.com/evelynaguilar.art?igsh=YzUzczhxeWJpY292&utm_source=qr";
 //const youtube = "https://www.youtube.com/@losmaitenes";
 /* const tiktok = "https://www.tiktok.com/";
@@ -13,8 +13,8 @@ const instagram = "https://www.instagram.com/evelynaguilar.art?igsh=YzUzczhxeWJp
 const FooterIcons = () => {
     return (
         <div className='footer__icons__container'>
-            {/* Brand icons */}
-            {/* <Link to={facebook} target='_blank'><FontAwesomeIcon className='footer__icons' icon={['fab', 'facebook']} /></Link> */}
+            {/* Brand icons */} {/* <FontAwesomeIcon className='footer__icons' icon={['fab', 'facebook']} /> */}
+            <Link to={artsy} target='_blank'><img src={artsyLogo} className='footer__icons artsy-logo' alt="artsy logo" /></Link>
             <Link to={instagram} target='_blank'><FontAwesomeIcon className='footer__icons' icon={['fab', 'instagram']} /></Link>
             <Link to={whatsapp} target='_blank'><FontAwesomeIcon className='footer__icons' icon={['fab', 'whatsapp']} /></Link>
             {/* <Link to={tiktok} target='_blank'><FontAwesomeIcon className='footer__icons' icon={['fab', 'tiktok']} /></Link> */}
