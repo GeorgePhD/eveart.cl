0b42102c1398f8089d41c69680bb44602798eca2
George <lets.devel0pme@gmail.com>
2024-06-05 16:16:54 -0400

    update: oleopastel gallery structure changed, map added

--- src/Components/OleoPastel/OleoPastel.jsx
+++ src/Components/OleoPastel/OleoPastel.jsx
@@ -1,16 +1,7 @@
 import './oleopastel.css';
-import Image1 from '../../images/oleo-pastel (1).png'
-import Image2 from '../../images/oleo-pastel (2).png'
-import Image3 from '../../images/oleo-pastel (3).png'
-import Image4 from '../../images/oleo-pastel (4).png'
-import Image5 from '../../images/oleo-pastel (5).png'
-import Image6 from '../../images/oleo-pastel (6).png'
-import Image7 from '../../images/oleo-pastel (7).png'
-import Image8 from '../../images/oleo-pastel (8).png'
-import Image9 from '../../images/oleo-pastel (9).png'
-import Image10 from '../../images/oleo-pastel (10).png'
 import { useEffect } from 'react';
 import ContactBtn from '../ContactBtn/ContactBtn'
+import { oleopastel } from '../../galleriesInfo/oleopastel';
 
 
 const Oleopastel = () => {
@@ -23,7 +14,7 @@ const Oleopastel = () => {
                 const itemTop = item.getBoundingClientRect().top;
     
                 // Adjust this value based on when you want the animation to start
-                if (itemTop < window.innerHeight * 0.5) {
+                if (itemTop < window.innerHeight * 0.3) {
                     item.style.opacity = 1;
                 }
             });
@@ -41,47 +32,18 @@ const Oleopastel = () => {
     return (
         <>
         <div className='grid__layout__oleopastel'>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image1} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua I, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image2} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua II, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image3} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua III, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image4} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua IV, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.jpg</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image5} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua IX, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image6} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua V, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image7} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua VI, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image8} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua VII, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image9} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua VIII, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.</p>
-            </section>
-            <section className="oleopastel__container">
-                <img className='oleopastel__img' src={Image10} alt="El vigilante" />
-                <p className="oleopastel__p">Agua sobre agua X, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm.</p>
-            </section>
+                {
+                    
+                oleopastel.map((oleopastel, index) => {
 
+                    return (
+                        <section className="oleopastel__container" key={index}>
+                        <img className='oleopastel__img' src={oleopastel.image} alt="oleopastel"/>
+                        <p className="oleopastel__p">{oleopastel.description}</p>
+                    </section>
+                    )
+                })
+            }
         </div>
         <ContactBtn />
         </>
---
+++ src/galleriesInfo/oleopastel.js
@@ -0,0 +1,23 @@
+import OLEOPASTEL_1 from "../images/oleopastel/oleo-pastel-1.png";
+import OLEOPASTEL_2 from "../images/oleopastel/oleo-pastel-2.png";
+import OLEOPASTEL_3 from "../images/oleopastel/oleo-pastel-3.png";
+import OLEOPASTEL_4 from "../images/oleopastel/oleo-pastel-4.png";
+import OLEOPASTEL_5 from "../images/oleopastel/oleo-pastel-5.png";
+import OLEOPASTEL_6 from "../images/oleopastel/oleo-pastel-6.png";
+import OLEOPASTEL_7 from "../images/oleopastel/oleo-pastel-7.png";
+import OLEOPASTEL_8 from "../images/oleopastel/oleo-pastel-8.png";
+import OLEOPASTEL_9 from "../images/oleopastel/oleo-pastel-9.png";
+import OLEOPASTEL_10 from "../images/oleopastel/oleo-pastel-10.png";
+
+export const oleopastel = [
+    {image: OLEOPASTEL_1, description: "Agua sobre agua I, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_2, description: "Agua sobre agua II, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_3, description: "Agua sobre agua III, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_4, description: "Agua sobre agua IV, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_5, description: "Agua sobre agua IX, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_6, description: "Agua sobre agua V, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_7, description: "Agua sobre agua VI, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_8, description: "Agua sobre agua VII, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_9, description: "Agua sobre agua VIII, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."},
+    {image: OLEOPASTEL_10, description: "Agua sobre agua X, de la Serie Paisajes Inmersos 2023, óleo pastel sobre cartulina, 24 x 34 cm."}
+]
