3b9dd9bf521d5621efbc148624ed8a03d7b6bddc
George <lets.devel0pme@gmail.com>
2024-07-18 07:53:36 -0400

    contactBtn section styles fixed

--- src/Components/ContactBtn/ContactBtn.jsx
+++ src/Components/ContactBtn/ContactBtn.jsx
@@ -4,14 +4,13 @@ import './contactbtn.css';
 const ContactBtn = () => {
 
     return (
-        <div>
+        <div className="contactBtn__container">
             <div className="contactBtn__subtitle__container">
                 <h2 className="contactBtn__subtitle">¿Interesado en mis obras? </h2>
             </div>
             <div className="contactBtn__section">
                 <p className="contactBtn__content">Puedes contactarme acá:</p>
                 <a href="mailto:eveart@evelynaguilarart.com" className='contact__btn'>enviar mensaje</a>
-
             </div>
         </div>
         
